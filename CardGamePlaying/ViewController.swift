7edc1e851513afe701c4da75305e6b40c6f10242
Oleksandr <alexandrph1997@hotmail.com>
2018-07-24 01:51:39 +0200

    Adding models playcard

--- CardGamePlaying/ViewController.swift
+++ CardGamePlaying/ViewController.swift
@@ -10,15 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var deck = playCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for _ in 1...10{
+            if let card = deck.draw(){
+                print("\(card)")
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
